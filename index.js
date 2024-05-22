82902b5daad37cdf959b5ac1792f40d37339faab
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-05-22 12:28:36 +0530

    change to api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-const authRoute = require("./routes/auth");
+const authRoute = require("./routes/api");
 const app = express();
 app.use(express.json());
 app.use(
@@ -20,7 +20,7 @@ app.use(
     credentials: true,
   })
 );
-app.use("/auth", authRoute);
+app.use("/api", authRoute);
 app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(port);
