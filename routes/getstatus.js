7e42a55d3d4bf1d85c904ddbca9ad9d80c8e4b78
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-06-01 18:58:15 +0530

    revert back to main

--- routes/getstatus.js
+++ routes/getstatus.js
@@ -11,33 +11,26 @@ const routerAbi = require("../constants/abi/Router.json"); // Load Router contra
 const offRampAbi = require("../constants/abi/OffRamp.json");
 
 const getStatus = async (chain, targetChain, messageId) => {
-  console.log(
-    `Checking status of message ${messageId} from ${chain} to ${targetChain}`
-  );
   const destinationRpcUrl = getProviderRpcUrl(targetChain);
   const sourceRpcUrl = getProviderRpcUrl(chain);
 
   // Initialize providers to connect to the blockchain networks.
-  console.log("Initializing providers...");
   const destinationProvider = new JsonRpcProvider(destinationRpcUrl);
   const sourceProvider = new JsonRpcProvider(sourceRpcUrl);
 
   // Get configuration for routers on both chains.
-  console.log("Getting router configuration...");
   const sourceRouterAddress = getRouterConfig(chain).router;
   const sourceChainSelector = getRouterConfig(chain).chainSelector;
   const destinationRouterAddress = getRouterConfig(targetChain).router;
   const destinationChainSelector = getRouterConfig(targetChain).chainSelector;
 
   // Instantiate router contracts with ethers for both source and destination chains.
-  console.log("Instantiating router contracts...");
   const sourceRouterContract = new ethers.Contract(
     sourceRouterAddress,
     routerAbi,
     sourceProvider
   );
   // Check if the destination chain is supported by the source chain's router.
-  console.log("Checking if chain is supported...");
   const isChainSupported = await sourceRouterContract.isChainSupported(
     destinationChainSelector
   );
@@ -45,7 +38,6 @@ const getStatus = async (chain, targetChain, messageId) => {
     throw new Error(`Lane ${chain}->${targetChain} is not supported`);
   }
 
-  console.log("Destination chain is supported");
   const destinationRouterContract = new ethers.Contract(
     destinationRouterAddress,
     routerAbi,
@@ -53,20 +45,14 @@ const getStatus = async (chain, targetChain, messageId) => {
   );
 
   // Fetch OffRamp contracts associated with the destination router contract.
-  console.log("Fetching offRamps...");
   const offRamps = await destinationRouterContract.getOffRamps();
   // Filter for the OffRamps that match the source chain.
-  console.log("Filtering offRamps...");
   const matchingOffRamps = offRamps.filter(
     (offRamp) => offRamp.sourceChainSelector.toString() === sourceChainSelector
   );
 
   // Check each matching OffRamp for the message ID.
-  console.log("Checking each offRamp for message ID...");
-  console.log(matchingOffRamps);
   for (const matchingOffRamp of matchingOffRamps) {
-    console.log("matching off ramp");
-    console.log(matchingOffRamp.offRamp);
     const offRampContract = new ethers.Contract(
       matchingOffRamp.offRamp,
       offRampAbi,
@@ -76,8 +62,7 @@ const getStatus = async (chain, targetChain, messageId) => {
     const events = await offRampContract.queryFilter(
       offRampContract.filters.ExecutionStateChanged(undefined, messageId)
     );
-    console.log("EVENTS");
-    console.log(events);
+
     if (events.length > 0) {
       // If events are found, log the status and exit.
       const { state } = events[0].args;
@@ -97,15 +82,24 @@ const getStatus = async (chain, targetChain, messageId) => {
   );
 };
 
-router.post("/get-status", async (req, res) => {
+router.post("/getstatus", async (req, res) => {
   const { chain, targetChain, messageId } = req.body;
   const status = await getStatus(chain, targetChain, messageId);
-  res.status(200).json({
-    status: status,
-    chain: chain,
-    targetChain: targetChain,
-    messageId: messageId,
-  });
+  if (status) {
+    res.status(200).json({
+      status: status,
+      chain: chain,
+      targetChain: targetChain,
+      messageId: messageId,
+    });
+  } else {
+    res.status(200).json({
+      status: `Either the message ${messageId} does not exist OR it has not been processed yet on destination chain`,
+      chain: chain,
+      targetChain: targetChain,
+      messageId: messageId,
+    });
+  }
 });
 
 module.exports = router;
